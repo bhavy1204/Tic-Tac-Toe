a9f11a79d9677e17b9091327445a11072bb2f754
bhavy1204 <bhavysarwa51@gmail.com>
2025-02-16 09:41:12 +0530

    made basic functionality

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ const music = new Audio("music.mp3");
 const turnSound = new Audio("ting.mp3");
 const gameOver = new Audio("gameover.mp3");
 
-let turn = "x";
+let turn = "X";
 
 const changeTurn = ()=>{
+
     return turn === "X"?"0":"X";
 }
 
 // Win condition
+
+const checkWin = ()=>{
+
+}
+
+// main functioning of game 
+
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector(".boxtext");
+    element.addEventListener("click",()=>{
+        if(boxtext.innerText === ""){
+            boxtext.innerText=turn;
+            turn = changeTurn();
+            turnSound.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
+        }
+    });
+});
